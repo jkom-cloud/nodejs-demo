d136b321976c1ee060a80a8cd8944089d87e5afc
tsq <1530234656@qq.com>
2018-01-17 16:14:04 +0800

    Update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const envMessage = process.env.MESSAGE || 'from dev message';
+console.log("envMessage", envMessage);
 const message = 'very good job';
 
 app.get('/', (req, res) => {
